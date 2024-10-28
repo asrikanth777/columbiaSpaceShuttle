75519c97223669c82203348d2ee2e70b56ec2a4a
asrikanth777 <as140@illinois.edu>
2024-10-27 19:53:33 -0500

    push1

---
+++ equations.py
@@ -0,0 +1,22 @@
+"""
+This is the equations file, which will be used to calculate trajectory
+- Kepler's Laws
+- Vis-viva Equation
+
+"""
+
+import numpy as np
+import matplotlib as mpl
+from scipy import constants as spcons
+from astropy import constants as apcons
+
+# Important Constants
+gravity = spcons.gravitational_constant # N*m^2 / kg^2
+earthMass = apcons.E
+
+
+# Vis-Viva Equation
+# the Equation is v = sqrt(mew(2/r - 1/a))
+# def VisViva():
+
+
